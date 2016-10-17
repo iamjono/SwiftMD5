005340bd90912ffbda46f24eb23cfa8bfcb37913
Jonathan Guthrie <jono@perfect.org>
2016-10-17 12:22:55 -0400

    changed format. gggrrrr

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,5 +1,6 @@
 import XCTest
-import SwiftMD5Tests
-var tests = [XCTestCaseEntry]()
-tests += SwiftMD5Tests.allTests()
-XCTMain(tests)
+@testable import SwiftMD5Tests
+
+XCTMain([
+	testCase(SwiftMD5Tests.allTests),
+])
